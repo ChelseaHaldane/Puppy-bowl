86d20b6dd2e571fa8a466041cce84348de030b10
Chelsea <angelofwarcraft@gmail.com>
2023-02-20 11:32:25 -0600

    commit to public

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,21 +1,33 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = "2211-ftb-et-web-ft";
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
-
 export const fetchAllPlayers = async () => {
-
+  try {
+    const response = await fetch(`${APIURL}players`);
+    const result = await response.json();
+    if (result.error) {
+      throw result.error;
+    }
+    return result.data.players;
+  } catch (error) {
+    console.error("Uh oh, trouble fetching!", error);
+  }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-
+  try {
+    const response = await fetch(`${APIURL}players/${playerId}`);
+    const result = await response.json();
+    if (result.error) {
+      throw result.error;
+    }
+    return result.data;
+  } catch (error) {
+    console.error("Uh oh, trouble fetching!", error);
+  }
 };
+export const addNewPlayer = async (playerObj) => {};
 
-export const addNewPlayer = async (playerObj) => {
-
-};
-
-export const removePlayer = async (playerId) => {
-
-};
+export const removePlayer = async (playerId) => {};
